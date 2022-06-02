f178b069822eb981a750858356f1d776d77a70e7
mary atieno <mercymary 948@gmail.com>
2022-06-02 11:09:47 +0300

    initial commit

---
+++ gallery/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+# from __future__ import unicode_literals
+
+
+class GalleryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gallery'
